adcc5a9d3156f5975f890dd9a0d6f8d0e2285931
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2025-09-22 20:10:53 +0530

    updated s3 bucket commands

--- miscellaneous/upload-2-s3-bucket.sh
+++ miscellaneous/upload-2-s3-bucket.sh
@@ -10,3 +10,11 @@ aws s3 ls s3://mys3bucketname/postgres-sample-databases/
 # upload contents of a local folder
 aws s3 sync /stale-storage/Softwares/PostgreSQL/PostgreSQL-Sample-Dbs s3://mys3bucketname/postgres-sample-databases/
 
+# Upload a file to S3
+aws s3 cp /path/to/local/file.txt s3://my-bucket-name/file.txt
+
+# aws s3 cp s3://my-bucket-name/file.txt /path/to/local/file.txt
+aws s3 cp s3://my-bucket-name/file.txt /path/to/local/file.txt
+
+
+
